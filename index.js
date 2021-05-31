b92d7dd0b35a1dc48f52ec9ab20b4355bceb7bcf
Divya Margaret <divya.margaret96@gmail.com>
2021-05-31 00:41:46 -0700

    db connected, mongoose , register section done

--- index.js
+++ index.js
@@ -1,72 +1,70 @@
-const session=require("express-session")
-const express=require("express");
-const dataservice=require("./service/dataservice");
-const app =express();
+const session = require("express-session")
+const express = require("express");
+const dataservice = require("./service/dataservice");
+const { User } = require("./service/db");
+const app = express();
 app.use(express.json());
 app.use(session({
-    secret:'randomsecurestring',
-    resave:false,
-    saveUninitialized:false
+    secret: 'randomsecurestring',
+    resave: false,
+    saveUninitialized: false
 }));
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     console.log("Middleware");
     next();
 });
 
-const logMiddleware = (req,res,next)=>{
+const logMiddleware = (req, res, next) => {
     console.log(req.body);
     next();
 }
 app.use(logMiddleware);
 
-const authMiddleware =(req,res,next)=>{
-    if(!req.session.currentUser){
+const authMiddleware = (req, res, next) => {
+    if (!req.session.currentUser) {
         return res.json({
-          statusCode:422,
-            status: false, 
-            message:"Please LOG IN"})
-    
-      }
-      next();
+            statusCode: 422,
+            status: false,
+            message: "Please LOG IN"
+        })
+
+    }
+    next();
 };
 
 
 
-app.get('/',(req,res)=>{res.send("this is a get method !!")});
+app.get('/', (req, res) => { res.send("this is a get method !!") });
 
-app.post('/',(req,res)=>{res.send("this is a post method !!")});
+app.post('/', (req, res) => { res.send("this is a post method !!") });
 
-app.post('/register',(req,res)=>
-{
-    const result=dataservice.register(req.body.acc,req.body.use,req.body.pass);
-  res.status(result.statusCode).json(result) ;
+app.post('/register', (req, res) => {
+    dataservice.register(req.body.acc, req.body.use, req.body.pass)
+        .then(result => (res.status(result.statusCode).json(result)));
 }
 );
-app.post('/login',(req,res)=>
-{
-    const result=dataservice.login(req,req.body.acc,req.body.use,req.body.pass);
+app.post('/login', (req, res) => {
+    const result = dataservice.login(req, req.body.acc, req.body.use, req.body.pass);
     res.status(result.statusCode).json(result)
 }
 );
-app.post('/withdraw',authMiddleware,(req,res)=>
-{
-    const result=dataservice.withdraw(req.body.acc,req.body.use,req.body.pass,req.body.amt);
-  res.status(result.statusCode).json(result);
+app.post('/withdraw', authMiddleware, (req, res) => {
+    const result = dataservice.withdraw(req.body.acc, req.body.use, req.body.pass, req.body.amt);
+    res.status(result.statusCode).json(result);
 }
 );
-app.post('/deposit',authMiddleware,(req,res)=>
-{
-    
-    const result=dataservice.deposit(req.body.acc,req.body.use,req.body.pass,req.body.amt);
+app.post('/deposit', authMiddleware, (req, res) => {
+
+    const result = dataservice.deposit(req.body.acc, req.body.use, req.body.pass, req.body.amt);
     res.status(result.statusCode).json(result);
 }
 );
 
 
-app.put('/',(req,res)=>{res.send("this is a put method !!")});
+app.put('/', (req, res) => { res.send("this is a put method !!") });
 
-app.patch('/',(req,res)=>{res.send("this is a patch method !!")});
+app.patch('/', (req, res) => { res.send("this is a patch method !!") });
 
-app.delete('/',(req,res)=>{res.send("this is a delete method !!")});
+app.delete('/', (req, res) => { res.send("this is a delete method !!") });
 
-app.listen(3000,()=>{console.log("server started at port:3000")});
+app.listen(3000, () => { console.log("server started at port:3000") });
--- service/dataservice.js
+++ service/dataservice.js
@@ -1,155 +1,169 @@
-userdetails={
-    1000:{acno:1000,actype:"savings",username:"userone",password:"userone",balance:50000},
-    1001:{acno:1001,actype:"savings",username:"usertwo",password:"usertwo",balance:5000},
-    1002:{acno:1002,actype:"current",username:"userthree",password:"userthree",balance:10000},
-    1003:{acno:1003,actype:"current",username:"userfour",password:"userfour",balance:6000}
+const db = require('./db');
+
+userdetails = {
+  1000: { acno: 1000, actype: "savings", username: "userone", password: "userone", balance: 50000 },
+  1001: { acno: 1001, actype: "savings", username: "usertwo", password: "usertwo", balance: 5000 },
+  1002: { acno: 1002, actype: "current", username: "userthree", password: "userthree", balance: 10000 },
+  1003: { acno: 1003, actype: "current", username: "userfour", password: "userfour", balance: 6000 }
 }
 
-const register=  (acc,use,pass) =>{
-    var user=userdetails;
+const register = (acno, use, pass) => {
+  return db.User.findOne({ acno })
+    .then(user => {
+      if (user) {
+        return {
+          statusCode: 422,
+          status: false,
+          message: "user already exists"
+        }
+      }
+      else {
+        const newUser = new db.User({
+          acno: acno,
+          username: use,
+          password: pass,
+          balance: 0
+        })
+        newUser.save();
+        return {
+          statusCode: 200,
+          status: true,
+          message: "succesfully registerd"
+        }
+      }
+    })
+}
 
-    if(acc in user){
+
+
+
+const login = (req, acc, use, pass) => {
+  var users = userdetails;
+  if (acc in users) {
+    if (use == users[acc]["username"] && pass == users[acc]["password"]) {
+      req.session.currentUser = users[acc];
+
       return {
-          statusCode:422,
-          status:false,
-          message:"user already exists"
-      }
-     
-    }
-    else{
-      user[acc]={
-        acc:acc,
-        username:use,
-        password:pass,
-        balance:0
-      }
-      
-      
-      return {
-        statusCode:200,
+        statusCode: 200,
         status: true,
-        message:"succesfully registerd"
-    }
-     
-    }
-};
+        message: "login succesful"
 
- 
-
-const login= (req,acc,use,pass) => {
-  var users= userdetails;
-  if(acc in users)
-  {
-      if(use==users[acc]["username"] && pass==users[acc]["password"]){
-        req.session.currentUser=users[acc];
-        
-        return {
-          statusCode:200,
-          status:true,
-          message: "login succesful"
-
-        }
-        
       }
 
-      else{
-        return {
-          statusCode:422,
-          status:false,
-          message: "Invalid username or password"
+    }
 
-        }
-          
+    else {
+      return {
+        statusCode: 422,
+        status: false,
+        message: "Invalid username or password"
+
       }
+
+    }
   }
-  else{
+  else {
     return {
-      statusCode:422,
-          status:false,
+      statusCode: 422,
+      status: false,
       message: "Invalid account number"
 
     }
-     
+
   }
 };
 
-const withdraw= (acc,use,pass,amt)=>{
+const withdraw = (acc, use, pass, amt) => {
 
 
-  
-  var amount=parseInt(amt);
-  let user=userdetails;
-  if(acc in user)
-  {
-    if(pass==user[acc]["password"] && use==user[acc]["username"]){
-      if(user[acc]["balance"] >= amount){
-      user[acc]["balance"]-=amount;
-      
-      return {
-        statusCode:200,
-        status: true, 
-        message:"your account has been debited with amount "+amt+" and your available balance is "+user[acc]["balance"]}}
-      else{
+
+  var amount = parseInt(amt);
+  let user = userdetails;
+  if (acc in user) {
+    if (pass == user[acc]["password"] && use == user[acc]["username"]) {
+      if (user[acc]["balance"] >= amount) {
+        user[acc]["balance"] -= amount;
+
         return {
-          statusCode:422,
-          status: false, 
-          message:"Insufficient Balance"}}
+          statusCode: 200,
+          status: true,
+          message: "your account has been debited with amount " + amt + " and your available balance is " + user[acc]["balance"]
+        }
       }
-      
-      else{
+      else {
         return {
-        statusCode:422,
-          status: false, 
-          message:"Invalid username or password"}} }
-    
+          statusCode: 422,
+          status: false,
+          message: "Insufficient Balance"
+        }
+      }
+    }
 
-      else{
-        return {
-          statusCode:422,
-            status: false, 
-            message:"Invalid account number"}}
-      
-  
-
-
-}
-const deposit= (acc,use,pass,amt)=>{
-
-  
-  var amount=parseInt(amt) ;
-  let user=userdetails;
- 
-  if(acc in user)
-  {
-    if(pass==user[acc]["password"] && use==user[acc]["username"]){
-      user[acc]["balance"]+=amount;
-      
+    else {
       return {
-        statusCode:200,
-        status: true, 
-        message:"your account has been credited with amount "+amt+" and your available balance is "+user[acc]["balance"]}}
-   
-      
-      
-      else{
-        return {
-        statusCode:422,
-          status: false, 
-          message:"Invalid username or password"}} }
-    
+        statusCode: 422,
+        status: false,
+        message: "Invalid username or password"
+      }
+    }
+  }
 
-      else{
-        return {
-          statusCode:422,
-            status: false, 
-            message:"Invalid account number"}}  
-      
-  
+
+  else {
+    return {
+      statusCode: 422,
+      status: false,
+      message: "Invalid account number"
+    }
+  }
+
+
+
+
+}
+const deposit = (acc, use, pass, amt) => {
+
+
+  var amount = parseInt(amt);
+  let user = userdetails;
+
+  if (acc in user) {
+    if (pass == user[acc]["password"] && use == user[acc]["username"]) {
+      user[acc]["balance"] += amount;
+
+      return {
+        statusCode: 200,
+        status: true,
+        message: "your account has been credited with amount " + amt + " and your available balance is " + user[acc]["balance"]
+      }
+    }
+
+
+
+    else {
+      return {
+        statusCode: 422,
+        status: false,
+        message: "Invalid username or password"
+      }
+    }
+  }
+
+
+  else {
+    return {
+      statusCode: 422,
+      status: false,
+      message: "Invalid account number"
+    }
+  }
+
+
 
 
 }
 
-module.exports={register,login,withdraw,deposit};
+module.exports = { register, login, withdraw, deposit };
 
 
 
